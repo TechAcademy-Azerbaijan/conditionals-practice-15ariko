dc2e7a5daa54465f6f710378a7870253330015c1
15ariko <110599821+15ariko@users.noreply.github.com>
2022-08-16 14:23:11 +0300

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,12 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+   let input = result.input.split("")
+
+    for (let i = 0; i < input.length; i++) {
+        if (parseInt(input[i]) === 3 && parseInt(input[i + 1]) === 7) {
+            console.log('YES');
+        } else { console.log('NO'); }
+
+    }  
 });
