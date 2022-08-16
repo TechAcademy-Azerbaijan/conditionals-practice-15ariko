4534ddaac8c981e079c85fb2530ef04e947451ce
15ariko <110599821+15ariko@users.noreply.github.com>
2022-08-16 14:21:40 +0300

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,13 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+let x = parseInt(result.input)
+    if (-10000 <= x && x <= 10000) {
+        if (x >= 10) {
+            let num = x ** 3 + x * 5;
+        } else {
+            num = x ** 2 - 2 * x + 4
+            console.log(num)
+        }
+    }  
 });
