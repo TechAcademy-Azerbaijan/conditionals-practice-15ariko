24d9489f94640986f5407ee85e465641fdb27f5a
15ariko <110599821+15ariko@users.noreply.github.com>
2022-08-16 14:23:47 +0300

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -4,6 +4,17 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+let n = result.input
+    let a = parseInt(n / 1000);
+    let b = parseInt((n / 100) % 10);
+    let c = parseInt((n / 10) % 10);
+    let d = parseInt(n % 10);
+
+    if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0 && d % 2 == 0) {
+        console.log('YES')
+
+    } else {
+        console.log('NO');
+
+    }  
 });
